c8e4148021524c4f5728852ac34a3cd876594430
IftakherEmon <iftakherhossain12345@gmail.com>
2026-07-04 16:02:32 +0600

    Implemented interactive main menu

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int choice;
+
+    while (true)
+    {
+        cout << "\n=====================================\n";
+        cout << "      STRING MANIPULATION TOOL\n";
+        cout << "=====================================\n";
+        cout << "1. Reverse String\n";
+        cout << "2. Convert to Uppercase\n";
+        cout << "3. Convert to Lowercase\n";
+        cout << "4. Count Vowels\n";
+        cout << "5. Count Words\n";
+        cout << "6. Check Palindrome\n";
+        cout << "7. Find String Length\n";
+        cout << "8. Exit\n";
+        cout << "=====================================\n";
+
+        cout << "Enter your choice: ";
+
+        if (!(cin >> choice))
+        {
+            cout << "\nInvalid input! Please enter a number between 1 and 8.\n";
+
+            cin.clear();
+            cin.ignore(1000, '\n');
+
+            continue;
+        }
+
+        if (choice >= 1 && choice <= 8)
+        {
+            if (choice == 8)
+            {
+                cout << "\nThank you for using the program!\n";
+                break;
+            }
+
+            cout << "\nFeature will be implemented in the next commits.\n";
+        }
+        else
+        {
+            cout << "\nInvalid choice! Please select between 1 and 8.\n";
+        }
+    }
+
+    return 0;
+}
